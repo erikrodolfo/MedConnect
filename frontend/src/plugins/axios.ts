e5f1e473961d9f6fc838bc9a4dab3198ded06a49
erik <erikrodolfo23@gmail.com>
2026-04-22 22:17:34 -0300

    preparar projeto para deploy

--- frontend/src/plugins/axios.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.interceptors.response.use((response) => {
-  return response;
-});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,54 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  //plugins
+  plugins: [vue()], //plugin do vue3
+  //base path - caminho na apliacação
+  base: "/", //saída -> https://meu-site.com/
+
+  //resolve - Alias
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)), //aponta para o 'src/' | usa '@/component em vez de ../../component
     },
   },
+  //build | produção
+  build: {
+    outDir: "dist", //pasta de saída do build
+    sourcemap: false, //gera arquivos .map para debug | false em produção (arquivos menores)
+    minify: "esbuild", //minifica código(retira espaços, comentários) | 'esbuild' é mais rápido que 'terser'
+    chunkSizeWarningLimit: 500, //avisa se chunck > 500
+
+    //configurações avançadas
+    rollupOptions: {
+      output: {
+        // manualChunks: Divide código em chunks separados
+        // Melhora cache (se Vue atualizar, só baixa chunk do Vue)
+        manualChunks: (id) => {
+          // Se o módulo está em node_modules
+          if (id.includes("node_modules")) {
+            // Chunk com Vue, Vue Router e Pinia
+            if (id.includes("vue") || id.includes("pinia")) {
+              return "vue-vendor";
+            }
+            // Chunk com Axios
+            if (id.includes("axios")) {
+              return "axios-vendor";
+            }
+          }
+        },
+      },
+    },
+  },
+  //server de desenvolvimento
+server: {
+  port: 5173, //porta do servidor de desenvolvimento
+  open: false, // Abre navegador automaticamente
+  cors: true, //Habilita CORS no dev server
+}
+
 });
+
