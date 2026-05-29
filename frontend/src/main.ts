36f0b71c1faff41cad4eaa459b4fc80a22fe9c3c
erik <erikrodolfo23@gmail.com>
2026-05-28 23:37:54 -0300

    fix: support frontend access vie local network IP

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,21 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
+
+//toastify notifications
 import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
+
+//pinia
 import { createPinia } from "pinia";
+
+//vue modal
 import { createVfm } from "vue-final-modal";
 import 'vue-final-modal/style.css'
+
+//vue router
 import router from "./router";
+
+// styles
 import "./styles/variables.css";
 import "./styles/reset.css";
 import "./styles/global.css";
@@ -16,13 +26,13 @@ const isMobile = window.innerWidth <= 768
 
 const app = createApp(App); //instãncia da aplicação
 
-app.use(createPinia())
+app.use(createPinia()) //pinia
 
-const vfm = createVfm();
+const vfm = createVfm(); //vue modal
 
 app.use(vfm);
 app.use(router);
-app.use(Vue3Toastify, {
+app.use(Vue3Toastify, { //toastify notifications
   autoClose: 2000,
   closeButton: !isMobile,
   position: isMobile ? 'top-center' : 'bottom-right',
